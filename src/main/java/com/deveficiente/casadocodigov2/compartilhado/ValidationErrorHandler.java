ae1c04561cf8199d3a660c3c1761cd560b0e87fc
alberto souza <alots.ssa@gmail.com>
2020-07-04 19:08:27 -0300

    [messages-properties] configurando para uso

--- src/main/java/com/deveficiente/casadocodigov2/compartilhado/ValidationErrorHandler.java
+++ src/main/java/com/deveficiente/casadocodigov2/compartilhado/ValidationErrorHandler.java
@@ -2,25 +2,32 @@ package com.deveficiente.casadocodigov2.compartilhado;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 @RestControllerAdvice
 public class ValidationErrorHandler {
 
     @Autowired
     private MessageSource messageSource;
+    
+	private static final Logger log = LoggerFactory
+			.getLogger(ValidationErrorHandler.class);
+
     
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -43,6 +50,20 @@ public class ValidationErrorHandler {
     	return buildValidationErrors(globalErrors,
 				fieldErrors);
     }
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ValidationErrorsOutputDto handleValidationError(HttpMessageNotReadableException exception) {
+    	log.error("Problema na de desserializar o objeto",exception);
+    	
+    	InvalidFormatException invalidFormat = (InvalidFormatException) exception.getCause();
+    	
+    	List<ObjectError> globalErrors = List.of(new ObjectError("", invalidFormat.getValue()+" não é um valor válido"));
+    	List<FieldError> fieldErrors = List.of();
+    	
+    	return buildValidationErrors(globalErrors,
+    			fieldErrors);
+    }
 
 	private ValidationErrorsOutputDto buildValidationErrors(List<ObjectError> globalErrors,
 			List<FieldError> fieldErrors) {
@@ -60,4 +81,6 @@ public class ValidationErrorHandler {
     private String getErrorMessage(ObjectError error) {
         return messageSource.getMessage(error, LocaleContextHolder.getLocale());
     }
+    
+    
 }
