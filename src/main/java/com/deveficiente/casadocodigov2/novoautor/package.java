b75ea58f27964810714e7789157981398826e66d
Alberto - Dev Eficiente <alberto@deveficiente.com>
2022-02-12 04:58:41 -0300

    refactor(camadas - novo autor) avaliacao da situacao atual

---
+++ src/main/java/com/deveficiente/casadocodigov2/novoautor/package.java
@@ -0,0 +1,10 @@
+/*
+ * Analise da funcionalidade pelo olhar das regras da arquitetura em camadas descritas
+ * por Bob Marting
+ * 
+ * Independent of Frameworks - Não (depende da JPA e também do Spring)
+ * Testable. The business rules can be tested without the UI, Database... - Não (depende da injecao do Spring de atributo privado)
+ * Independent of UI - Não(só funciona por enquanto se for via web)
+ * Independent of Database - Não (depende da JPA)
+ * Independent of any external agency - Não (depende do banco de dados, dado que está acoplada a JPA)
+ */
