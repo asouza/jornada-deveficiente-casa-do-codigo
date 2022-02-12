fa8dd7f2729ae92fa30fad9c10cbd4a418ec703e
Alberto - Dev Eficiente <alberto@deveficiente.com>
2022-02-12 06:31:11 -0300

    refactor(camadas - novo autor) suporta um pouco de self testing com o validated do spring

--- src/main/java/com/deveficiente/casadocodigov2/novoautor/CadastroNovoAutor.java
+++ src/main/java/com/deveficiente/casadocodigov2/novoautor/CadastroNovoAutor.java
@@ -4,10 +4,14 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 @Service
+@Validated
+//3
 public class CadastroNovoAutor {
 
+	//1
 	private AutorRepository autorRepository;
 
 	public CadastroNovoAutor(AutorRepository autorRepository) {
@@ -16,6 +20,7 @@ public class CadastroNovoAutor {
 	}
 
 	@Transactional
+	//2
 	public Autor executa(@Valid DadosNovoAutor dados) {
 		Autor autor = dados.toModel();
 		autorRepository.save(autor);
