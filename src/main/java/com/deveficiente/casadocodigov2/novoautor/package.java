c8ec27459a9a572a4884e3b6e54653b7b8ecd202
Alberto - Dev Eficiente <alberto@deveficiente.com>
2022-02-12 05:01:51 -0300

    refactor(camadas - novo autor) atualizando avaliacao

--- src/main/java/com/deveficiente/casadocodigov2/novoautor/package.java
+++ src/main/java/com/deveficiente/casadocodigov2/novoautor/package.java
@@ -3,7 +3,7 @@
  * por Bob Marting
  * 
  * Independent of Frameworks - Não (depende da JPA e também do Spring)
- * Testable. The business rules can be tested without the UI, Database... - Não (depende da injecao do Spring de atributo privado)
+ * Testable. The business rules can be tested without the UI, Database... - Sim(só mockar entitymanager)
  * Independent of UI - Não(só funciona por enquanto se for via web)
  * Independent of Database - Não (depende da JPA)
  * Independent of any external agency - Não (depende do banco de dados, dado que está acoplada a JPA)
