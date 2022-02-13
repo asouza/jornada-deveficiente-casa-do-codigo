bb155f490a37263e3cf89da85dc9cb16a6a43e93
Alberto - Dev Eficiente <alberto@deveficiente.com>
2022-02-13 07:04:57 -0300

    refactor(camadas - nova categoria) registrando os principios e resultados que precisam ser seguidos

---
+++ src/main/java/com/deveficiente/casadocodigov2/cadastrocategoria/package.java
@@ -0,0 +1,14 @@
+/*
+ * Analise da funcionalidade pelo olhar das regras da arquitetura em camadas descritas
+ * por Bob Marting
+ * 
+ * Independent of Frameworks - Não
+ * Testable. The business rules can be tested without the UI, Database... - Não
+ * Independent of UI - Não
+ * Independent of Database - Não
+ * Independent of any external agency - Sim(desconsiderando o banco de dados)
+ * 
+ * Principios:
+ * 
+ * 1 - Segue a The Dependency Rule - Sim 
+ */
