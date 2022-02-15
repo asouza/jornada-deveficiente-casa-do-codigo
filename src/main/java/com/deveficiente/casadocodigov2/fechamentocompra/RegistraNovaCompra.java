d1f1165a6f3802b53e3c942fbeb7a43b71e784d5
Alberto - Dev Eficiente <alberto@deveficiente.com>
2022-02-15 07:48:50 -0300

    refactor(camadas - nova compra) checando pre condicao para executar o registro da compra mesmo

--- src/main/java/com/deveficiente/casadocodigov2/fechamentocompra/RegistraNovaCompra.java
+++ src/main/java/com/deveficiente/casadocodigov2/fechamentocompra/RegistraNovaCompra.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.deveficiente.casadocodigov2.cadastrolivro.BuscadorDeEntidades;
 
@@ -16,18 +15,32 @@ public class RegistraNovaCompra {
 	private CompraRepository compraRepository;
 	private CupomRepository cupomRepository;
 	private BuscadorDeEntidades buscadorDeEntidades;
+	private ValidaEstadoPertenceAPais validaEstadoPertenceAPais;
+	private ValidaCumpomValido validaCumpomValido;
 
 	public RegistraNovaCompra(CompraRepository compraRepository,
 			CupomRepository cupomRepository,
-			BuscadorDeEntidades buscadorDeEntidades) {
+			BuscadorDeEntidades buscadorDeEntidades,
+			ValidaEstadoPertenceAPais validaEstadoPertenceAPais,
+			ValidaCumpomValido validaCumpomValido) {
 		super();
 		this.compraRepository = compraRepository;
 		this.cupomRepository = cupomRepository;
 		this.buscadorDeEntidades = buscadorDeEntidades;
+		this.validaEstadoPertenceAPais = validaEstadoPertenceAPais;
+		this.validaCumpomValido = validaCumpomValido;
 	}
 
 	@Transactional
 	public Compra executa(@Valid DadosNovaCompra dados) {
+		
+		validaCumpomValido.valida(dados, () -> {
+			throw new IllegalArgumentException("Neste ponto do fluxo o cupom devia estar valido");
+		});
+		
+		validaEstadoPertenceAPais.valida(dados, () -> {
+			throw new IllegalArgumentException("Neste ponto do fluxo a validacao do estado pertencente ao pais deveria estar feita");
+		});
 
 		Compra novaCompra = dados.toModel(buscadorDeEntidades, cupomRepository);
 		return compraRepository.save(novaCompra);
